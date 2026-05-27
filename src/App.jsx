d937eff22dba00d55ded1fcc3d51e9a484d60e34
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-05-28 01:07:12 +0900

    add: 原罪の軸をuseStateで管理

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Graph from "./components/Graph";
 import Settings from "./components/Settings";
 
 export default function App() {
+  const [axis, setAxis] = useState({
+    x: "sepal_length",
+    y: "sepal_width"
+  });
+
   return (
     <div>
-      <Settings />
-      <Graph />
+      <Settings axis={axis} onAxisChanged={setAxis} />
+      <Graph axis={axis} />
     </div>
   );
 }
