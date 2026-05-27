5bc7b35c8c31d5fb8005addb4155ea58c9429e6f
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-05-28 03:00:26 +0900

    add: グラフ描画用のプログラムを追加

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,10 +1,53 @@
+import { useMemo } from "react";
+import * as d3 from "d3";
+
 export default function Graph({ axis, data }) {
-    const width = 600;
-    const height = 600;
+    const width = 500;
+    const height = 500;
+    
+    const plotWidth = width - 70;
+    const plotHeight = height - 70;
+
+    const plotData = { x: [], y: [] };
+    for (const obj of data) {
+        plotData.x.push(obj[axis.x]);
+        plotData.y.push(obj[axis.y]);
+    }
+
+    const xScale = useMemo(() => {
+        const [min, max] = d3.extent(plotData.x);
+        return d3.scaleLinear().domain([min ?? 0, max ?? 1]).nice().range([0, plotWidth]);
+    }, [plotData]);
+    const yScale = useMemo(() => {
+        const [min, max] = d3.extent(plotData.y);
+        return d3.scaleLinear().domain([min ?? 0, max ?? 1]).nice().range([plotHeight, 0]);
+    }, [plotData]);
+
+    const xTicks = useMemo(() => xScale.ticks(10), [xScale]);
+    const yTicks = useMemo(() => yScale.ticks(10), [yScale]);
 
     return (
         <div>
-            <p>X: {axis.x} / Y: {axis.y}</p>
+            <svg width={width} height={height}>
+                <g transform={`translate(50,50)`}>
+                    <line x1="0" y1={plotHeight} x2={plotWidth} y2={plotHeight} stroke="black" />
+                    <line x1="0" y1="0" x2="0" y2={plotHeight} stroke="black" />
+
+                    {xTicks.map((tick) => (
+                        <g key={`x-${tick}`} transform={`translate(${xScale(tick)}, ${plotHeight})`}>
+                            <line x1="0" y1="0" x2="0" y2="6" stroke="black" />
+                            <text y="20" textAnchor="middle" fontSize="10">{tick}</text>
+                        </g>
+                    ))}
+
+                    {yTicks.map((tick) => (
+                        <g key={`y-${tick}`} transform={`translate(0, ${yScale(tick)})`}>
+                            <line x1="0" y1="0" x2="-6" y2="0" stroke="black" />
+                            <text x="-10" textAnchor="end" dominantBaseline="middle" fontSize="10">{tick}</text>
+                        </g>
+                    ))}
+                </g>
+            </svg>
         </div>
     );
 }
